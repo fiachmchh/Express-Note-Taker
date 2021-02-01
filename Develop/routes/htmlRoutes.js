f918b9c3b71670188fbb8bf61b05e2af07aeabee
fiachmchh <fiachmchh@gmail.com>
2021-02-01 13:35:00 +0000

    code cleaned up

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,20 +1,7 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
+  
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
     
@@ -24,5 +11,4 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  
 };
